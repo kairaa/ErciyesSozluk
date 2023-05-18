cb3bee221f3b023f8043e1846d6016bfd5b2bb13
Kayra Çakıroğlu <1030510099@erciyes.edu.tr>
2023-05-18 23:24:17 +0300

    WebApp tarafinda entry ve entrycomment ile ilgili islemler tamamlandi

--- src/Api/WebApi/ErciyesSozluk.Api.WebApi/Controllers/VoteController.cs
+++ src/Api/WebApi/ErciyesSozluk.Api.WebApi/Controllers/VoteController.cs
@@ -11,7 +11,7 @@ namespace ErciyesSozluk.Api.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class VoteController : BaseController
     {
         private readonly IMediator mediator;
